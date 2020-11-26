04bcfa72f5c5ddde9a2af28b0324b8968464b945
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-11-26 08:33:33 -0500

    Height

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - checks if a node is a leaf
+ * binary_tree_postorder - checks if a node is a leaf
  * @tree: is a pointer to the root node of the tree to traverse
  * @func: is a pointer to a function to call for each node.
  * The value in the node must be passed as a parameter to this function.
