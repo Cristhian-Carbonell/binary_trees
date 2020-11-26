940e1df22afe0fe3f58b9ddc6b844074d6093b7a
Cristhian-Carbonell <alexcarbo8809@gmail.com>
2020-11-26 08:26:10 -0500

    Pre-order traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - checks if a node is a leaf
+ * @node: node to be checked
+ *
+ * Return: 1 if node is a leaf. 0 otherwise or in case of error
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
