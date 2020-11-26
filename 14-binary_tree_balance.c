eab981a7d876ed25be92d80f13a5eff5be9b2449
serosc95 <oscardleon95@gmail.com>
2020-11-26 23:50:38 +0000

    balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -15,8 +15,11 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 	if (tree->left)
 		b_left += 1 + binary_tree_balance(tree->left);
+	else
+		b_left = 1;
 	if (tree->right)
 		b_right += 1 + binary_tree_balance(tree->right);
-
+	else
+		b_right = 1;
 	return (b_left - b_right);
 }
